a66c17257f26f8236eeca71494b5b8cfd149e03c
innurhap28 <kmugi118@gmail.com>
2026-07-26 16:11:17 +0900

    feat: css 보완 및 navbar.js 작성

---
+++ js/components/navbar.js
@@ -0,0 +1,8 @@
+export function initNavbar() {
+    const menuToggle = document.querySelector("#menu-toggle");
+    const navMenu = document.querySelector(".nav-menu");
+
+    menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+}
